81404927b3491588fe4202d853acae4baeb05627
gitamadoudiene <amadoudiene20@gmail.com>
2024-07-25 02:34:21 +0000

    Mbiir mi 02:34

--- pages/services/index.js
+++ pages/services/index.js
@@ -10,11 +10,25 @@ import { fadeIn } from '../../variants'
 const Services = () => {
   return <div className="h-full bg-primary/30 py-36 flex items-center">
     
-      {/* <ServiceSlider /> */}
+      
       
       <Circles />
-      <div className="container mx-auto ">
-      </div>
+      <div className="container mx-auto">
+  <div className='flex flex-col xl:flex-row gap-x-8'>
+    {/* text  */}
+    <div className='text-center flex xl:w-[300px] flex-col lg:text-left mb-4 xl:mb-8'>
+      <h2 className='h2 xl:mt-8 flex items-center justify-center lg:justify-start whitespace-nowrap'>
+        My Services<span className='text-accent'>.</span>
+      </h2>
+      <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+        I am a full-stack developer
+      </p>
+    </div>
+  </div>
+  {/* Services */}
+  <ServiceSlider />
+</div>
+
       <Bulb />
      
   </div>;
