88d23fc4b54b9ce1f013904c7ba87286914f7f17
gitamadoudiene <amadoudiene20@gmail.com>
2024-07-29 12:42:33 +0000

    Service Matna

--- components/ServiceSlider.js
+++ components/ServiceSlider.js
@@ -1,14 +1,21 @@
-// icons
+// Import Swiper styles
+import 'swiper/swiper-bundle.min.css';
+
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode, Pagination } from 'swiper';
+
+// Import icons
 import {
   RxCrop,
   RxPencil2,
   RxDesktop,
   RxReader,
   RxRocket,
-} from "react-icons/rx";
+  RxArrowTopRight,
+} from 'react-icons/rx';
 
-
-// data
+// Data
 const serviceData = [
   {
     icon: <RxCrop />,
@@ -38,7 +45,51 @@ const serviceData = [
 ];
 
 const ServiceSlider = () => {
-  return <div>Service Slider</div>;
+  return (
+    <Swiper
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 15,
+        },
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 15,
+        },
+        1440: {
+          slidesPerView: 2,
+          spaceBetween: 15,
+        },
+      }}
+      freeMode={true}
+      pagination={{ clickable: true }}
+      modules={[FreeMode, Pagination]}
+      className='h-[240px] sm:h-[340px]'
+    >
+      {serviceData.map((item, index) => (
+        <SwiperSlide key={index}>
+          <div className='bg-[rgba(65,47,123,0.15)] h-max rounded-lg px-6 py-8 
+          flex sm:flex-col gap-x-6 sm:gap-x-0 group cursor-pointer  hover:bg-[rgba(89,65,169,0.15)] transition-all duration-300'>
+            {/* icons */}
+            <div className='text-4xl text-accent mb-4 '>{item.icon}</div>
+            {/* title and description */}
+            <div className='mb-8'>
+              <div className='mb-2 text-lg'>{item.title}</div>
+              <p className='max-w-[350px] leading-normal '>{item.description}</p>
+            </div>   
+            {/* arrow */}
+            <div className="text-3xl">
+              <RxArrowTopRight className='group-hover:rotate-45 group-hover:text-accent transition-all duration-300'/>
+            </div>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
 };
 
 export default ServiceSlider;
--- pages/services/index.js
+++ pages/services/index.js
@@ -1,37 +1,51 @@
-//import components
-import ServiceSlider from '../../components/ServiceSlider'
-import Bulb from '../../components/Bulb'
-import Circles from '../../components/Circles'
+// Import components
+import ServiceSlider from '../../components/ServiceSlider';
+import Bulb from '../../components/Bulb';
+import Circles from '../../components/Circles';
 
-//Framer-motion
-import {motion} from 'framer-motion'
-import { fadeIn } from '../../variants'
+// Framer-motion
+import { motion } from 'framer-motion';
+import { fadeIn } from '../../variants';
 
 const Services = () => {
-  return <div className="h-full bg-primary/30 py-36 flex items-center">
-    
-      
-      
+  return (
+    <div className="h-full bg-primary/30 py-36 flex items-center">
       <Circles />
       <div className="container mx-auto">
-  <div className='flex flex-col xl:flex-row gap-x-8'>
-    {/* text  */}
-    <div className='text-center flex xl:w-[300px] flex-col lg:text-left mb-4 xl:mb-8'>
-      <h2 className='h2 xl:mt-8 flex items-center justify-center lg:justify-start whitespace-nowrap'>
-        My Services<span className='text-accent'>.</span>
-      </h2>
-      <p className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
-        I am a full-stack developer
-      </p>
-    </div>
-  </div>
-  {/* Services */}
-  <ServiceSlider />
-</div>
-
+        <div className='flex flex-col xl:flex-row gap-x-8'>
+          {/* text */}
+          <div className='text-center flex xl:w-[30%] flex-col lg:text-left mb-4 xl:mb-0'>
+            <motion.h2 
+            variants={fadeIn('up', 0.2)}
+            initial='hidden'
+            animate='show'
+            exit='hidden'
+             className='h2 xl:mt-8 flex items-center justify-center lg:justify-start whitespace-nowrap'>
+              My Services<span className='text-accent'>.</span>
+            </motion.h2>
+            <motion.p
+             variants={fadeIn('up', 0.4)}
+             initial='hidden'
+             animate='show'
+             exit='hidden'
+            className='mb-4 max-w-[400px] mx-auto lg:mx-0'>
+              I am a full-stack developer
+            </motion.p>
+          </div>
+          {/* Services */}
+          <motion.div 
+           variants={fadeIn('down', 0.6)}
+           initial='hidden'
+           animate='show'
+           exit='hidden'
+          className='w-full xl:max-w-[65%]'>
+            <ServiceSlider />
+          </motion.div>
+        </div>
+      </div>
       <Bulb />
-     
-  </div>;
+    </div>
+  );
 };
 
 export default Services;
