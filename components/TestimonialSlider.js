f1aebb4006e7b0a97bfdda3a85c2a38f43501c3c
gitamadoudiene <amadoudiene20@gmail.com>
2024-08-01 02:11:55 +0000

    Testimonial Matna

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -25,53 +25,58 @@ const testimonialSlider = [
 
 // Import Swiper styles
 import 'swiper/css';
-import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper';
+import {Navigation, Pagination } from 'swiper';
 import Image from 'next/image';
 
 // icons
-import {
-  RxCrop,
-  RxPencil2,
-  RxDesktop,
-  RxReader,
-  RxRocket,
-  RxArrowTopRight,
-} from 'react-icons/rx';
-
-import { BsArrowRight } from 'react-icons/bs';
+import { FaQuoteLeft } from 'react-icons/fa';
 
 const TestimonialSlider = () => {
   return (
     <Swiper
       spaceBetween={10}
+      navigation={true}
       pagination={{ clickable: true }}
-      modules={[Pagination]}
-      className='h-[280px] sm:h-[480px]'
+      modules={[Navigation,Pagination]}
+      className='h-[400px]'
     >
       {testimonialSlider.map((person, index) => (
         <SwiperSlide key={index}>
-          <div>
+          <div className='flex flex-col items-center md:flex-row gap-x-8 h-full px-16'>
             {/* avatar, nom, position  */}
-            <div>
-              <div>
+            <div className='w-full max-w-[300px] flex flex-col xl:justify-center
+            items-center relative mx-auto xl:mx-0'>
+              <div className='flex flex-col justify-center text-center'>
                 {/* avatar  */}
-                <div> avatar image</div>
+                <div className='mb-2 mx-auto'> 
+                 <Image src={person.image} width={100} height={100} alt=''/>
+                  </div>
                 {/* nom  */}
-                <div> Nom</div>
+                <div className='text-lg'> {person.name}</div>
                 {/* position  */}
-                <div> position </div>
+                <div className='text-[12px] uppercase font-extralight
+                tracking-widest'> {person.position}</div>
               </div>
 
             </div>
             {/* cote et message  */}
-            <div>
-              <div>quote and message</div>
-            </div>
+            <div className=' flex-1 flex flex-col justify-center before:w-[1px] xl:before:bg-white/20
+            xl:before:absolute xl:before:left-0 xl:before:h-[200px] relative xl:pl-20' >
+                    {/* quotes icons */} 
+                    <div className='mb-4'>
+                       <FaQuoteLeft className='text-4xl xl:text-6xl text-white/20 mx-auto 
+                       md:mx-0'/>
+                     </div>
+                     {/* message  */}
+                     <div className='xl:text-lg text-center md:text-left'
+                     >{person.message}</div>
+              </div>     
+           
           </div>
          
         </SwiperSlide>
